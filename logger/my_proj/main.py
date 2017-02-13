4e7639eaf53b228a1149610b5178c757d59f1466
Raghavan Viswanathan <raghavan@riptideio.com>
2017-02-13 19:07:37 +0530

    master: Fixed pep8

--- logger/my_proj/main.py
+++ logger/my_proj/main.py
@@ -1,9 +1,9 @@
 import logging
 from colorlog import ColoredFormatter
 from my_proj.m1.m1_src import m1f
-from my_proj.m2.m2_src import m2f
 
-def setup_logger():
+
+def setup_root_logger():
     formatter = ColoredFormatter(
         '%(log_color)s%(asctime)s:%(levelname)s:%(name)s:%(lineno)s'
         ':%(reset)s%(message)s'
@@ -15,16 +15,14 @@ def setup_logger():
     logger.addHandler(handler)
     logger.setLevel(logging.DEBUG)
 
+
 def main():
-    setup_logger()
+    setup_root_logger()
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.ERROR)
-    logger.critical("Appearing since log level for {} >= ERROR".format(__name__))
-    logger.error("Appearing since log level for {} >= ERROR".format(__name__))
-    logger.warning("Not appearing since log level for {} >= ERROR".format(__name__))
-    logger.info("Not appearing since log level for {} >= ERROR".format(__name__))
-    logger.debug("Not appearing since log level for {} >= ERROR".format(__name__))
+    logger.critical("Appearing coz log level for {} >= ERROR".format(__name__))
+    logger.error("Appearing coz log level for {} >= ERROR".format(__name__))
+    logger.warning("Not appearing coz log level for {} >= ERROR".format(__name__))
+    logger.info("Not appearing coz log level for {} >= ERROR".format(__name__))
+    logger.debug("Not appearing coz log level for {} >= ERROR".format(__name__))
     m1f()
-    print logging.getLogger().manager.loggerDict.keys()
-
-
